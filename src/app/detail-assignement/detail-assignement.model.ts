92c2cde8965732fb562fc5c3d76b333eb6bf2ac0
10soa <soaornella5@gmail.com>
2024-04-13 14:13:04 +0300

    fiche detail assignement

---
+++ src/app/detail-assignement/detail-assignement.model.ts
@@ -0,0 +1,13 @@
+export class DetailAssignement {
+  idEleve?: string;
+  note?: Number;
+  remarque?: String;
+  dateRenduEleve?: Date;
+  rendu?: Boolean;
+  eleve?: string;
+  matiere?: "Programmation parallèle";
+  niveau?: "Licence_3 (L3)";
+  assignement?: string;
+  dateRendu?: Date;
+  idAssignement?: string;
+}
