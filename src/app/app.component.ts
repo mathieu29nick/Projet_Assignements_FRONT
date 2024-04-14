de00dcd85e8904f9b1859864490ff47d5e2ada8d
10soa <soaornella5@gmail.com>
2024-04-15 01:57:19 +0300

    valider devoir drag and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { HomepageComponent } from './homepage/homepage.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,HomepageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Assignements_soa_nick';
